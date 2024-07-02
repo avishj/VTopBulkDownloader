89aad62e7a598d25cb074f79932f700935464441
Avish Jha <avish.j@protonmail.com>
2024-07-02 22:01:52 +0530

    add base logic to load till login page
    Signed-off-by: Avish Jha <avish.j@protonmail.com>

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
 import puppeteer from "puppeteer";
-
 // Launching the browser
 const browser = await puppeteer.launch({
-	headless: false,
-	args: [`--start-maximized`]
+    headless: false,
+    args: [`--start-maximized`]
 });
-
 // Setting up the page
 const vtop = await browser.newPage();
 vtop.setViewport({
-	width: await vtop.evaluate(() => window.screen.width),
-	height: await vtop.evaluate(() => window.screen.height)
+    width: await vtop.evaluate(() => window.screen.width),
+    height: await vtop.evaluate(() => window.screen.height)
 });
-
 // Navigating to the page and performing login
 await vtop.goto("https://vtop.vit.ac.in/vtop/");
 await vtop.evaluate(`submitForm('stdForm');`);
