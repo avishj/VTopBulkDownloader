3b27e29e0769648b7b32a7ebeac187bc09661118
Avish Jha <avish.j@protonmail.com>
2024-07-05 22:31:59 +0530

    feat: create logger util
    Signed-off-by: Avish Jha <avish.j@protonmail.com>

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,10 @@
+import { Context } from "./enums";
+
 export default {
 	async sleep(ms: number) {
 		return new Promise((resolve) => setTimeout(resolve, ms));
+	},
+	async log(context: Context, message: string) {
+		console.log(`[${new Date().toLocaleString()}] - [${context}] - ${message}`);
 	}
 };
