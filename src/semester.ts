d45b74fe40529a7950595d4b1bcb494044c47f2a
Avish Jha <avish.j@protonmail.com>
2024-07-05 22:12:20 +0530

    refactor: semester to use types and improve naming
    Signed-off-by: Avish Jha <avish.j@protonmail.com>

--- src/semester.ts
+++ src/semester.ts
@@ -1,33 +1,41 @@
 import { Page } from "puppeteer";
 import utils from "./utils.js";
+import { Course, Semester } from "./types.js";
 
-async function extractAllSemesters(vtop: Page) {
+async function extractSemesters(vtop: Page): Promise<Semester[]> {
 	return await vtop.evaluate(() => {
-		return (Array.from(document.querySelectorAll('select[id="semesterSubId"] option[value*="VL"]')) as HTMLOptionElement[]).map((e) => e.value);
+		const elements: HTMLOptionElement[] = Array.from(document.querySelectorAll('select[id="semesterSubId"] option[value*="VL"]'));
+		return elements.map((e) => {
+			return {
+				name: e.innerText,
+				hasAssignments: null,
+				value: e.value,
+				courses: []
+			};
+		});
 	});
 }
-async function selectSemester(vtop: Page, value: string) {
-	await vtop.select('select[id="semesterSubId"]', value);
+async function selectSemester(vtop: Page, semester: Semester): Promise<void> {
+	await vtop.select('select[id="semesterSubId"]', semester.value);
 	await vtop.waitForNetworkIdle();
 }
-async function hasAssignments(vtop: Page) {
+async function hasCourses(vtop: Page): Promise<boolean> {
 	return await vtop.evaluate(() => {
 		return document.querySelectorAll("#fixedTableContainer").length > 0;
 	});
 }
-async function extractAllAssignments(vtop: Page) {
+async function extractCourses(vtop: Page): Promise<Course[]> {
 	return await vtop.evaluate(() => {
-		const assignments = {} as { [key: string]: any };
+		const assignments: Course[] = [];
 		document.querySelectorAll("#fixedTableContainer tr.tableContent").forEach((row) => {
 			const tds = row.querySelectorAll("td");
-			const key = tds[0].innerText.trim();
-			assignments[key] = {
+			assignments.push({
 				classNumber: tds[1].innerText.trim(),
 				courseCode: tds[2].innerText.trim(),
 				courseTitle: tds[3].innerText.trim(),
 				courseType: tds[4].innerText.trim(),
 				facultyName: tds[5].innerText.trim()
-			};
+			});
 		});
 		return assignments;
 	});
@@ -35,15 +43,14 @@ async function extractAllAssignments(vtop: Page) {
 
 export default {
 	async main(vtop: Page) {
-		const semesters = await extractAllSemesters(vtop);
-
+		const semesters = await extractSemesters(vtop);
 		for (const semester of semesters) {
 			await selectSemester(vtop, semester);
-			if (await hasAssignments(vtop)) {
+			if (await hasCourses(vtop)) {
 				console.log(semester + " has assignments");
 				await vtop.waitForNetworkIdle();
 				await utils.sleep(1000);
-				const assignments = await extractAllAssignments(vtop);
+				const assignments = await extractCourses(vtop);
 				console.log(assignments);
 			} else {
 				console.log(semester + " has no assignments");
