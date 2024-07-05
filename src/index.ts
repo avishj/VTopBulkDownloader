f590cd7f75b140a223385723f2809fa527b4fb69
Avish Jha <avish.j@protonmail.com>
2024-07-05 17:49:45 +0530

    feat: add all sems
    Signed-off-by: Avish Jha <avish.j@protonmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 import setup from "./setup.js";
 import da from "./da.js";
+import utils from "./utils.js";
 
 // Loading the environment variables
 dotenv.config();
@@ -14,12 +15,18 @@ async function main() {
 	const semesters = await da.semester.extractAllSemesters(vtop);
 	console.log(semesters);
 
-	await da.semester.selectSemester(vtop, semesters[3]);
-
-	if (await da.semester.hasAssignments(vtop)) {
-		console.log("has assignments");
-		const assignments = await da.semester.extractAllAssignments(vtop);
-		console.log(assignments);
+	for (const semester of semesters) {
+		await da.semester.selectSemester(vtop, semester);
+		if (await da.semester.hasAssignments(vtop)) {
+			console.log(semester + " has assignments");
+			await vtop.waitForNetworkIdle();
+			await utils.sleep(1000);
+			const assignments = await da.semester.extractAllAssignments(vtop);
+			console.log(assignments);
+		} else {
+			console.log(semester + " has no assignments");
+		}
+		await utils.sleep(3000);
 	}
 }
 
