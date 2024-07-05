c57363a27d74ab1a7bf32e58b2cf2fce6738b7f7
Avish Jha <avish.j@protonmail.com>
2024-07-05 17:50:25 +0530

    try: puppeteer $$
    Signed-off-by: Avish Jha <avish.j@protonmail.com>

--- src/da.ts
+++ src/da.ts
@@ -17,20 +17,35 @@ export default {
 			});
 		},
 		async extractAllAssignments(vtop: Page) {
-			return await vtop.evaluate(() => {
-				const assignments = new Map();
-				document.querySelectorAll("#fixedTableContainer tr.tableContent").forEach((row) => {
-					const tds = row.querySelectorAll("td");
-					assignments.set(tds[0].innerText.trim(), {
-						classNumber: tds[1].innerText.trim(),
-						courseCode: tds[2].innerText.trim(),
-						courseTitle: tds[3].innerText.trim(),
-						courseType: tds[4].innerText.trim(),
-						facultyName: tds[5].innerText.trim()
-					});
+			const assignments = new Map();
+			(await vtop.$$("#fixedTableContainer tr.tableContent")).forEach(async (row) => {
+				const tds = await row.$$("td");
+				console.log(tds);
+				console.log(await tds[0].getProperties());
+				assignments.set((await tds[0].getProperty("innerText")).toString().trim(), {
+					classNumber: (await tds[1].getProperty("innerText")).toString().trim(),
+					courseCode: (await tds[2].getProperty("innerText")).toString().trim(),
+					courseTitle: (await tds[3].getProperty("innerText")).toString().trim(),
+					courseType: (await tds[4].getProperty("innerText")).toString().trim(),
+					facultyName: (await tds[5].getProperty("innerText")).toString().trim()
 				});
-				return assignments;
 			});
+			// const e = await vtop.evaluate(() => {
+			// 	document.querySelectorAll("#fixedTableContainer tr.tableContent").forEach((row) => {
+			// 		const tds = row.querySelectorAll("td");
+			// 		assignments.set(tds[0].innerText.trim(), {
+			// 			classNumber: tds[1].innerText.trim(),
+			// 			courseCode: tds[2].innerText.trim(),
+			// 			courseTitle: tds[3].innerText.trim(),
+			// 			courseType: tds[4].innerText.trim(),
+			// 			facultyName: tds[5].innerText.trim()
+			// 		});
+			// 	});
+			// 	console.log(assignments);
+			// 	return assignments;
+			// });
+			console.log(assignments);
+			return assignments;
 		}
 	}
 	// assignment: {
