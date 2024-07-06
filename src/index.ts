e9f0bfcdfb303f503ed4526c82fd70b3a0034442
Avish Jha <avish.j@protonmail.com>
2024-07-06 21:02:20 +0530

    feat: add dir creation
    Signed-off-by: Avish Jha <avish.j@protonmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import dotenv from "dotenv";
 import setup from "./setup.js";
 import semester from "./semester.js";
+import directory from "./directory.js";
 
 // Loading the environment variables
 dotenv.config();
 
 async function main() {
+	const basePath = await directory.init();
 	const vtop = await setup.main();
 	await semester.main(vtop);
 }
