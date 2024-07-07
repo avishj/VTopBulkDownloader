8e0519d20797bf475af14d968b350d99f008a5ec
Avish Jha <avish.j@protonmail.com>
2024-07-07 14:18:48 +0530

    feat: refactor directory to new structure
    Signed-off-by: Avish Jha <avish.j@protonmail.com>

--- src/directory.ts
+++ src/directory.ts
@@ -1,42 +1,49 @@
 import { Context } from "./enums.js";
+import { Course, Output, Semester } from "./types.js";
 import utils from "./utils.js";
 import fs from "fs-extra";
-import nodePath from "node:path";
+import path from "node:path";
 
 const logger = utils.log.logger.bind(null, Context.Directory);
 let basePath: string;
 
-export default {
-	async createFolderAt(path: string, name: string) {
-		await fs.ensureDir(nodePath.join(path, name));
-		logger(`Created folder: ${name}`);
-		await process.chdir(path);
-	},
-	async createFoldersAt(path: string, folders: string[]) {
-		for (const folder of folders) {
-			await this.createFolderAt(path, folder);
-		}
-	},
-	async getWorkingDirectory(): Promise<string> {
+export const core = {
+	getWorkingDirectory(): string {
 		logger(`Current working directory: ${process.cwd()}`);
 		return process.cwd();
+	}
+};
+
+export const output = {
+	async create() {
+		basePath = core.getWorkingDirectory();
+		fs.ensureDirSync(path.join(basePath, "output"));
+		logger(`Created output folder!`);
 	},
-	async setWorkingDirectory(path: string) {
-		logger(`Set working directory to: ${path} from ${process.cwd()}!`);
-		process.chdir(path);
+	async write(output: Output) {
+		await fs.writeFile(path.join(basePath, "output.json"), JSON.stringify(output, null, 4));
+		logger(`Wrote output file!`);
+	}
+};
+
+export const semester = {
+	async create(semester: Semester) {
+		await fs.ensureDir(path.join(basePath, "output", semester.name));
+		logger(`Created semester folder: ${semester.name}!`);
 	},
-	async init() {
-		logger("Initializing the output directory!");
-		basePath = await this.getWorkingDirectory();
-		await this.createFolderAt(basePath, "output");
-		basePath = nodePath.join(basePath, "output");
-		await this.setWorkingDirectory(basePath);
+	async write(semester: Semester) {
+		await fs.writeFile(path.join(basePath, "output", semester.name, "semester.json"), JSON.stringify(semester, null, 4));
+		logger(`Wrote semester file: ${semester.name}!`);
+	}
+};
+
+export const course = {
+	async create(semester: Semester, course: Course) {
+		await fs.ensureDir(path.join(basePath, "output", semester.name, course.courseCode + " - " + course.courseTitle));
+		logger(`Created course folder: ${course.courseCode}!`);
 	},
-	async getBasePath() {
-		return basePath;
-	},
-	async writeFile(path: string, name: string, content: string) {
-		await fs.writeFile(nodePath.join(path, name), content);
-		logger(`Wrote file: ${nodePath.join(path, name)}!`);
+	async write(semester: Semester, course: Course) {
+		await fs.writeFile(path.join(basePath, "output", semester.name, course.courseCode + " - " + course.courseTitle, "course.json"), JSON.stringify(course, null, 4));
+		logger(`Wrote course file: ${course.courseCode} - ${course.courseTitle}!`);
 	}
 };
