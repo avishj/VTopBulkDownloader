28ba3bb4819e00ad6fd6fedb3470020945043372
Avish Jha <avish.j@protonmail.com>
2024-07-07 20:32:25 +0530

    feat: create output logic for refactored version
    Signed-off-by: Avish Jha <avish.j@protonmail.com>

---
+++ src/output.ts
@@ -0,0 +1,53 @@
+import { Page } from "puppeteer";
+import { Context } from "./utils/enums.js";
+import { Output, Semester } from "./utils/types.js";
+import log from "./utils/log.js";
+import { output as directory } from "./directory.js";
+import helpers from "./utils/helpers.js";
+import semester from "./semester.js";
+
+const logger = log.logger.bind(null, Context.Output);
+
+const internal = {
+	async extractSemesters(vtop: Page): Promise<Semester[]> {
+		logger("Extracting semesters!");
+		const semesters = await vtop.evaluate(() => {
+			const elements: HTMLOptionElement[] = Array.from(document.querySelectorAll('select[id="semesterSubId"] option[value*="VL"]'));
+			return elements.map((e) => {
+				return {
+					name: e.innerText,
+					value: e.value,
+					courses: []
+				};
+			});
+		});
+		logger(`Found ${semesters.length} semesters!`);
+		await vtop.waitForNetworkIdle();
+		return semesters;
+	},
+	async selectSemester(vtop: Page, semester: Semester): Promise<void> {
+		logger(`${semester.name} - ${semester.value} is being selected!`);
+		await vtop.select('select[id="semesterSubId"]', semester.value);
+		logger(`${semester.name} - ${semester.value} has been selected!`);
+		await vtop.waitForNetworkIdle();
+	}
+};
+
+export default {
+	async main(timestamp: string, vtop: Page) {
+		logger("Starting!");
+		directory.create();
+		const output: Output = {
+			timestamp: timestamp,
+			semesters: []
+		};
+		output.semesters = await internal.extractSemesters(vtop);
+		for (let i = 0; i < output.semesters.length; i++) {
+			await internal.selectSemester(vtop, output.semesters[i]);
+			output.semesters[i] = await semester.main(vtop, output.semesters[i], i === output.semesters.length - 1);
+			await helpers.sleep(1000);
+		}
+		await directory.write(output);
+		logger("Done!");
+	}
+};
